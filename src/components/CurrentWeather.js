6be26da03f818fcd6ecdc8b72985c5ae5cdcb1be
Cemre Aktemur <cemre_aktemur@hotmail.com>
2021-01-16 00:31:23 +0300

    city name düzeltildi

--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -11,10 +11,11 @@ function Current() {
       <div className="weather-icon row">
         <i className="wi wi-day-sunny"></i>
       </div>
+      <h3 className="location-name row">{cityName}</h3>
       <h1 className="temp-c row">{results[0].temp} ºC</h1>
       <h5 className="condition row">{results[0].dayLong}</h5>
       <h5 className="condition row">{results[0].description}</h5>
-      <h3 className="location-name row">{cityName}</h3>
+     
       <h5 className="location-country row">
         En Yüksek Sıcaklık: {results[0].maxTemp}{" "}
       </h5>
